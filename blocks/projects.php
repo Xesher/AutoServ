f59c459285017d149fe86efbe5fcec00cd2f6dc6
Xesher <feniks4913@yandex.ru>
2017-06-02 22:28:07 +0300

    new

---
+++ blocks/projects.php
@@ -0,0 +1,32 @@
+<?php $title='Наши проекты'; $id='project';?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../style/mine.css">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <main>
+        <div class="content">
+            <div class="article">
+                <h1>Название статьи</h1>
+                <img src="../img/hero1.jpg">
+                <p>Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи Текст статьи </p>
+            </div>
+        </div>
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="#">Новые проекты</a> </li>
+                    <li><a href="#">Топ просмотров</a> </li>
+<!--                    <li><a href="#"></a> </li>-->
+<!--                    <li><a href="#"></a> </li>-->
+<!--                    <li><a href="#"></a> </li>-->
+<!--                    <li><a href="#"></a> </li>-->
+                 </ul>
+            </nav>
+        </aside>
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php $title='Главная'; $id='index'; require ('blocks/head.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/mine.css" title="none" media="screen" charset="utf-8" type="text/css">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <header id="header">
+        <nav class="navbar">
+            <div class="container-fluid" >
+                <ul class="nav-pillss">
+                    <li class="nav-unit"><a href="#">Главная</a></li>
+                    <li class="nav-unit"><a href="blocks/projects.php">Наши проекты</a></li>
+                    <li class="navbar-brand" ><img src="img/logo.png" title="AvtoSTO" alt="AvtoSTO"></li>
+                    <li class="nav-unit"><a href="#">Где мы находимся?</a></li>
+                    <li class="nav-unit"><a href="#">О нас</a></li>
+                </ul>
+             </div>
+        </nav>
+        <div class="hero_img"> <img src="img/hero2.jpg"></div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+
+<!--Скрипты-->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+</body>
+</html>
