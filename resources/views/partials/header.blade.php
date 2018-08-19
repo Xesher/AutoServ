4703b24510ed4853a1304337c02a5f720c82709c
Xesher <feniks4913@yandex.ru>
2018-08-19 23:34:38 +0300

    redesign nav and full header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,32 @@
-<header id="header" class="container-fluid">
-        <ul class="nav d-flex justify-content-around">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Главная</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Дианостика</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Услуи и сервис </a>
-            </li>
-            <li class="nav-item">
-                <img class="nav-item--img" src="{{asset('img/logo.png')}}" alt="">
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Кузовные работы</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Контакты</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">О компании</a>
-            </li>
-        </ul>
+<header class="container-fluid header">
+    <div class="d-flex justify-content-around align-items-center header__info">
+        <address class="header__address"><img src="" alt="">Вологда, Северная улица д. 15 стр. 4</address>
+        <p class="header__time"><img src="" alt="">Мы работаем c 10.00 до 20.00 Без выходных</p>
+    </div>
+    <div class="container header__body d-flex justify-content-between align-items-center">
+        <img class="header__logo" src="{{asset('img/logo.png')}}" alt="">
+        <p class="header__slogan">Полный цикл <br> Ремонт и обслуживание</p>
+        <a href="#" class="btn btn-primary header__call-me"> Заказ звонка</a>
+        <a class="header__phone" href="tel:+79004275889" >+7 (900) 427-58-89</a>
+    </div>
+    <ul class="nav container d-flex justify-content-around">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Главная</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Дианостика</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Услуги и сервис </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Кузовные работы</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Контакты</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">О компании</a>
+        </li>
+    </ul>
 </header>
