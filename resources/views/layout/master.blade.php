cfa61fc19cdb237959fd0335d07e2878f3b22058
Xesh_ <feniks4913@yandex.ru>
2018-08-20 11:49:13 +0300

    make slider

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    {{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 @include('partials.header')
@@ -13,8 +13,6 @@
     @yield('container')
 </main>
 @include('partials.footer')
-{{--<script src="{{ mix('js/manifest.js') }}"></script>--}}
-{{--<script src="{{ mix('js/vendor.js') }}"></script>--}}
 <script src="{{ mix('js/app.js') }}"></script>
 @stack('scripts')
 </body>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,37 @@
 @extends('layout.master')
 
 @section('container')
+    <div class="container-fluid index">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                </div>
+                <div class="carousel-item">
+                </div>
+                <div class="carousel-item">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    @push('scripts')
+        <script>
+            $('.carousel').carousel({
+                interval: 2000
+            })
+        </script>
 
+    @endpush
 @endsection
